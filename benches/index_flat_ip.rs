ad0a220db88af84e973dbd76c5a69aa614e26384
Karl Higley <kmhigley@gmail.com>
2022-09-23 12:41:07 -0400

    Split the benchmark suites for inner product and l2 distance indexes

--- benches/index_flat_ip.rs
+++ benches/index_flat_ip.rs
@@ -2,7 +2,6 @@ extern crate latus;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use latus::indexes::index_flat_ip::IndexFlatIP;
-// use latus::indexes::index_flat_l2::IndexFlatL2;
 use latus::primitives::vector::random_vector;
 
 fn index_flat_ip_query(index: &mut IndexFlatIP, k: usize, dim: usize) {
@@ -41,24 +40,19 @@ fn index_flat_ip_chunked_matrix_query(index: &mut IndexFlatIP, k: usize, dim: us
     );
 }
 
-// fn index_flat_l2_query(index: &IndexFlatL2, k: usize, dim: usize) {
-//     let query_vector = random_vector(dim);
-//     index.query(&query_vector, k);
-// }
-
-fn indexes_benchmark(c: &mut Criterion) {
-    let index_size = 1_048_576;
-    let dim = 128;
-    let k = 100;
+fn query_benchmark(c: &mut Criterion) {
+    let index_size = 1_183_514;
+    let dim = 100;
+    let k = 10;
 
     let mut index_ip: IndexFlatIP = IndexFlatIP::new(dim, false);
-    let mut index_ip_matrix: IndexFlatIP = IndexFlatIP::new(dim, false);
+    // let mut index_ip_matrix: IndexFlatIP = IndexFlatIP::new(dim, false);
     let mut index_ip_chunked: IndexFlatIP = IndexFlatIP::new(dim, true);
 
     for _ in 0..index_size {
         let vector = random_vector(dim);
         index_ip.insert(&vector.clone());
-        index_ip_matrix.insert(&vector.clone());
+        // index_ip_matrix.insert(&vector.clone());
         index_ip_chunked.insert(&vector.clone());
     }
 
@@ -66,15 +60,15 @@ fn indexes_benchmark(c: &mut Criterion) {
         b.iter(|| index_flat_ip_query(black_box(&mut index_ip), black_box(k), black_box(dim)))
     });
 
-    c.bench_function("index_flat_ip matrix query", |b| {
-        b.iter(|| {
-            index_flat_ip_matrix_query(
-                black_box(&mut index_ip_matrix),
-                black_box(k),
-                black_box(dim),
-            )
-        })
-    });
+    // c.bench_function("index_flat_ip matrix query", |b| {
+    //     b.iter(|| {
+    //         index_flat_ip_matrix_query(
+    //             black_box(&mut index_ip_matrix),
+    //             black_box(k),
+    //             black_box(dim),
+    //         )
+    //     })
+    // });
 
     c.bench_function("index_flat_ip chunked matrix query", |b| {
         b.iter(|| {
@@ -85,18 +79,7 @@ fn indexes_benchmark(c: &mut Criterion) {
             )
         })
     });
-
-    // let mut index_l2: IndexFlatL2 = IndexFlatL2::new(dim, false);
-    // index_l2.insert_many(&vectors);
-
-    // c.bench_function("index_flat_l2 query", |b| {
-    //     b.iter(|| index_flat_l2_query(black_box(&index_l2), black_box(k), black_box(dim)))
-    // });
-
-    // c.bench_function("index_flat_l2 matrix query", |b| {
-    //     b.iter(|| index_flat_l2_query(black_box(&index_l2), black_box(k), black_box(dim)))
-    // });
 }
 
-criterion_group!(benches, indexes_benchmark);
+criterion_group!(benches, query_benchmark);
 criterion_main!(benches);
---
+++ benches/index_flat_l2.rs
@@ -0,0 +1,55 @@
+extern crate latus;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use latus::indexes::index_flat_l2::IndexFlatL2;
+use latus::primitives::vector::random_vector;
+
+fn index_flat_l2_query(index: &IndexFlatL2, k: usize, dim: usize) {
+    let query_vector = random_vector(dim);
+    index.query(&query_vector, k);
+}
+
+fn query_benchmark(c: &mut Criterion) {
+    let index_size = 1_183_514;
+    let dim = 100;
+    let k = 10;
+
+    let mut index_l2: IndexFlatL2 = IndexFlatL2::new(dim);
+    // let mut index_l2_matrix: IndexFlatL2 = IndexFlatL2::new(dim, false);
+    // let mut index_l2_chunked: IndexFlatL2 = IndexFlatL2::new(dim);
+
+    for _ in 0..index_size {
+        let vector = random_vector(dim);
+        index_l2.insert(&vector.clone());
+        // index_l2_matrix.insert(&vector.clone());
+        index_l2_chunked.insert(&vector.clone());
+    }
+
+    c.bench_function("index_flat_l2 query", |b| {
+        b.iter(|| index_flat_l2_query(black_box(&mut index_l2), black_box(k), black_box(dim)))
+    });
+
+    // c.bench_function("index_flat_l2 matrix query", |b| {
+    //     b.iter(|| {
+    //         index_flat_l2_matrix_query(
+    //             black_box(&mut index_l2_matrix),
+    //             black_box(k),
+    //             black_box(dim),
+    //         )
+    //     })
+    // });
+
+    // c.bench_function("index_flat_l2 chunked matrix query", |b| {
+    //     b.iter(|| {
+    //         index_flat_l2_chunked_matrix_query(
+    //             black_box(&mut index_l2_chunked),
+    //             black_box(k),
+    //             black_box(dim),
+    //         )
+    //     })
+    // });
+}
+
+criterion_group!(benches, query_benchmark);
+criterion_main!(benches);
